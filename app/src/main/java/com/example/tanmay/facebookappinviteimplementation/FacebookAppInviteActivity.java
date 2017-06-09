984a92fe55484da3120f560b28fb25af4d6a510a
Tanmay Jha <tanmay.jha1@gmail.com>
2017-06-09 10:07:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tanmay/facebookappinviteimplementation/FacebookAppInviteActivity.java
@@ -0,0 +1,13 @@
+package com.example.tanmay.facebookappinviteimplementation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FacebookAppInviteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook_app_invite);
+    }
+}
